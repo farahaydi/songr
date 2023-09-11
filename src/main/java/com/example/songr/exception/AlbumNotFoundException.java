62254ac0c97cbc16470d49ef3e3f554e8b052799
farahisJoy <farahelaydi9@gmail.com>
2023-09-11 10:35:31 +0000

    songer13

---
+++ src/main/java/com/example/songr/exception/AlbumNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.songr.exception;
+
+public class AlbumNotFoundException extends RuntimeException{
+
+    public AlbumNotFoundException(String message) {
+        super(message);
+    }
+
+    public AlbumNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/example/songr/model/Song.java
@@ -0,0 +1,66 @@
+package com.example.songr.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private int length;
+    private int trackNumber;
+    @ManyToOne
+    private Album albumy;
+
+    public Song() {
+
+    }
+
+    public Song( String title, int length, int trackNumber, Album album) {
+        this.title = title;
+        this.length = length;
+        this.trackNumber = trackNumber;
+        this.albumy = album;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public Album getAlbum() {
+        return albumy;
+    }
+
+    public void setAlbum(Album album) {
+        this.albumy = album;
+    }
+}
